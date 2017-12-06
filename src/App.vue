5009bb08895edbdcf5c7689ea2ac4a5f24d94975
Feats Choi <feats.choi@gmail.com>
2017-12-06 12:24:47 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<h3>Example Vue App</h3>
+		<Clap></Clap>
+	</div>
+</template>
+
+<script>
+	import Clap from './Clap.vue'
+	export default {
+    name: 'app',
+		components: {
+			Clap
+		}
+  }
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
